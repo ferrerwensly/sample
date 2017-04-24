d22dae2ecd9e0a646e0ffc87b201ad50a26e8347
baisjeremiah <baisjeremaih@gmail.com>
2017-04-24 14:17:16 +0800

    may nabago

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("HELLO");
+            Console.WriteLine(".......");
+            Console.ReadKey();
         }
     }
 }
